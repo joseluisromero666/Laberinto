4f95824d75af0081557263c3f64cad89e80ebf7a
josel.romerom <josel.romerom@konradlorenz.edu.co>
2022-03-06 23:26:12 -0500

    add two sounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,40 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+
     public float speed;
-    private AudioSource audioRecoleccion;
 
+    //private AudioSource audioRecoleccion;
+    public GameObject SonidoMoneda;
 
+    public GameObject SonidoPared;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        audioRecoleccion=GetComponent<AudioSource>();
-
-        
+        // audioRecoleccion = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movimiento * speed);
-        
     }
+
     void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.CompareTag("RecolectablePositivo"))
         {
-
+            Instantiate (SonidoMoneda);
             Destroy(other.gameObject);
-               audioRecoleccion.Play();
-     
+            //   audioRecoleccion.Play();
         }
-
-
+        if (other.gameObject.CompareTag("Pared"))
+        {
+            Instantiate (SonidoPared);
+            //   audioRecoleccion.Play();
+        }
     }
 }
---
+++ Assets/Scripts/TiempoVidaSonido.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiempoVidaSonido : MonoBehaviour
+{
+    public float tiempoVidaSonido;
+
+    void Start()
+    {
+        Destroy (gameObject, tiempoVidaSonido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
