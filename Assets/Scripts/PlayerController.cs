ca02ecd786336558615becd53d88837ac0029543
josel.romerom <josel.romerom@konradlorenz.edu.co>
2022-03-10 00:44:21 -0500

    More Coins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,60 +1,112 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-  private Rigidbody rb;
+    private Rigidbody rb;
 
-  public float speed;
+    public float speed;
 
-  //private AudioSource audioRecoleccion;
-  public GameObject SonidoMoneda;
+    //private AudioSource audioRecoleccion;
+    public GameObject SonidoMoneda;
 
-  public GameObject SonidoPared;
-  public GameObject rampa;
+    public GameObject SonidoMonedaNegativa;
 
-  public bool rampaDestruida = false;
-  void Start()
-  {
-    rb = GetComponent<Rigidbody>();
-    // audioRecoleccion = GetComponent<AudioSource>();
-    rampa = GameObject.FindWithTag("Rampa");
-    Debug.Log(rampa);
-  }
+    public GameObject SonidoMonedaTiempo;
 
-  void Update()
-  {
-    float moveHorizontal = Input.GetAxis("Horizontal");
-    float moveVertical = Input.GetAxis("Vertical");
-    Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
-    rb.AddForce(movimiento * speed);
-  }
+    public GameObject SonidoMonedaTiempoNegativo;
 
-  void OnTriggerEnter(Collider other)
-  {
+    public GameObject SonidoPared;
 
-    if (other.gameObject.CompareTag("RecolectablePositivo"))
+    public GameObject SonidoDesaparecer;
+
+    public GameObject SonidoGanador;
+
+    private GameObject rampa;
+
+    private int puntos;
+
+    private float StartTime;
+
+    public Text textoPuntos;
+
+    public Text textoStartTime;
+
+    private bool rampaDestruida = false;
+
+    void Start()
     {
-      Instantiate(SonidoMoneda);
-      Destroy(other.gameObject);
-      //   audioRecoleccion.Play();
-    }
-    if (other.gameObject.CompareTag("Pared"))
-    {
-      Instantiate(SonidoPared);
-      //   audioRecoleccion.Play();
-    }
-    if (other.gameObject.CompareTag("Tablero"))
-    {
-      if (!rampaDestruida)
-      {
-        rampa.SetActive(false);
-        rampaDestruida = true;
-        Debug.Log("Destruyendo");
-      }
+        rb = GetComponent<Rigidbody>();
+
+        // audioRecoleccion = GetComponent<AudioSource>();
+        rampa = GameObject.FindWithTag("Rampa");
+        Debug.Log (rampa);
+        StartTime = Time.time;
+        puntos = 0;
     }
 
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movimiento * speed);
+        StartTime += 1 * Time.deltaTime;
+        textoStartTime.text = "Tiempo : " + StartTime.ToString();
+    }
 
-  }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("RecolectablePositivo"))
+        {
+            Instantiate (SonidoMoneda);
+            puntos += 100;
+            textoPuntos.text = "Puntos : " + puntos.ToString();
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("RecolectableNegativo"))
+        {
+            Instantiate (SonidoMonedaNegativa);
+            puntos -= 50;
+            textoPuntos.text = "Puntos : " + puntos.ToString();
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("RecolectableTiempo"))
+        {
+            Instantiate (SonidoMonedaTiempo);
+            StartTime -= 10;
+            textoStartTime.text = "Tiempo : " + StartTime.ToString();
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("RecolectableTiempoNegativo"))
+        {
+            Instantiate (SonidoMonedaTiempoNegativo);
+            StartTime += 20;
+            textoStartTime.text = "Tiempo : " + StartTime.ToString();
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("Llave"))
+        {
+            Instantiate (SonidoGanador);
+            Destroy(other.gameObject);
+            SceneManager.LoadScene(1);
+        }
+        if (other.gameObject.CompareTag("Pared"))
+        {
+            Instantiate (SonidoPared);
+        }
+        if (other.gameObject.CompareTag("Tablero"))
+        {
+            if (!rampaDestruida)
+            {
+                rampa.SetActive(false);
+                rampaDestruida = true;
+                Instantiate (SonidoDesaparecer);
+                Debug.Log("Destruyendo");
+            }
+        }
+    }
 }
