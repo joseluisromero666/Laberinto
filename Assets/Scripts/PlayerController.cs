6b5ebf438057b4a2331a610b16db89106a38907f
lionns <juans.leonv@icloud.com>
2022-03-09 21:25:49 -0500

    Agregando rampa al primer nivel, está se destruye al momento de caer al tablero

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,41 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
+  private Rigidbody rb;
 
-    public float speed;
+  public float speed;
 
-    //private AudioSource audioRecoleccion;
-    public GameObject SonidoMoneda;
+  //private AudioSource audioRecoleccion;
+  public GameObject SonidoMoneda;
 
-    public GameObject SonidoPared;
+  public GameObject SonidoPared;
+  public GameObject rampa;
 
-    void Start()
+  public bool rampaDestruida = false;
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    // audioRecoleccion = GetComponent<AudioSource>();
+    rampa = GameObject.FindWithTag("Rampa");
+    Debug.Log(rampa);
+  }
+
+  void Update()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+    Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    rb.AddForce(movimiento * speed);
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+
+    if (other.gameObject.CompareTag("RecolectablePositivo"))
     {
-        rb = GetComponent<Rigidbody>();
-        // audioRecoleccion = GetComponent<AudioSource>();
+      Instantiate(SonidoMoneda);
+      Destroy(other.gameObject);
+      //   audioRecoleccion.Play();
+    }
+    if (other.gameObject.CompareTag("Pared"))
+    {
+      Instantiate(SonidoPared);
+      //   audioRecoleccion.Play();
+    }
+    if (other.gameObject.CompareTag("Tablero"))
+    {
+      if (!rampaDestruida)
+      {
+        rampa.SetActive(false);
+        rampaDestruida = true;
+        Debug.Log("Destruyendo");
+      }
     }
 
-    void Update()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movimiento * speed);
-    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("RecolectablePositivo"))
-        {
-            Instantiate (SonidoMoneda);
-            Destroy(other.gameObject);
-            //   audioRecoleccion.Play();
-        }
-        if (other.gameObject.CompareTag("Pared"))
-        {
-            Instantiate (SonidoPared);
-            //   audioRecoleccion.Play();
-        }
-    }
+  }
 }
